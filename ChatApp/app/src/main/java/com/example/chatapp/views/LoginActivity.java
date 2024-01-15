5cd4b3df31b363b68f56f644ed2288fa6618a4f4
Orvet Priimägi <orvet.priimagi@gmail.com>
2024-01-15 13:16:55 +0200

    whatsapp clone - using Firebase and MVVM and Machine Learning Kit - Image to Text

---
+++ ChatApp/app/src/main/java/com/example/chatapp/views/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.example.chatapp.views;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import com.example.chatapp.R;
+import com.example.chatapp.databinding.ActivityLoginBinding;
+import com.example.chatapp.viewModel.MyViewModel;
+
+public class LoginActivity extends AppCompatActivity {
+
+    MyViewModel viewModel;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        viewModel = new ViewModelProvider(this).get(MyViewModel.class);
+
+
+        ActivityLoginBinding activityLoginBinding =
+                DataBindingUtil.setContentView(this, R.layout.activity_login);
+
+        activityLoginBinding.setVModel(viewModel);
+
+
+
+    }
+}
