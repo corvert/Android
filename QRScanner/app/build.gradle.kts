f9ec07bcac7ada9fee5b315d902d93721b5c2726
Orvet Priimägi <orvet.priimagi@gmail.com>
2024-01-23 18:00:30 +0200

    QR Code Scanner - Firebase ML Kit

--- QRScanner/app/build.gradle.kts
+++ QRScanner/app/build.gradle.kts
@@ -36,4 +36,20 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+// Use this dependency to use the dynamically downloaded model in Google Play Services
+    implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
+
+    //CameraX
+    implementation("androidx.camera:camera-core:1.3.1")
+    implementation("androidx.camera:camera-camera2:1.3.1")
+    implementation("androidx.camera:camera-lifecycle:1.3.1")
+    implementation("androidx.camera:camera-video:1.3.1")
+
+    implementation("androidx.camera:camera-view:1.3.1")
+    implementation("androidx.camera:camera-extensions:1.3.1")
+
+    implementation ("com.google.mlkit:barcode-scanning:17.2.0")
+
+    implementation("androidx.concurrent:concurrent-futures:1.1.0")
 }
